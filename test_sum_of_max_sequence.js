95b43dd2b7fcd335d3f5b9f772c099be3654b5f1
Ankoncha <44019225+luciferankon@users.noreply.github.com>
2019-05-25 11:38:56 +0530

    added test cases and changed function name

--- test_sum_of_max_sequence.js
+++ test_sum_of_max_sequence.js
@@ -1,7 +1,7 @@
-const { findSumOfMaxSubSequence } = require("./sum_of_max_sequence");
+const { findSumOfMaxSequence } = require("./sum_of_max_sequence");
 
 const test = function(numbers, expected) {
-  const actual = findSumOfMaxSubSequence(numbers);
+  const actual = findSumOfMaxSequence(numbers);
   if (expected == actual) return "pass";
   return "fail";
 };
@@ -11,3 +11,5 @@ console.log(test([1, 2, 2], 5));
 console.log(test([1, -1, 2], 2));
 console.log(test([-1, 1, -2], 1));
 console.log(test([1, -2, 3], 3));
+console.log(test([2, 3, -6, 4], 5));
+console.log(test([2, 3, -1, 4], 8));
